5cbf68899ffd4de254d2d901654e5a9d82f683ad
Abirami <abirami@bitcot.com>
2019-08-07 23:14:16 +0530

    active class bug fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,12 +12,28 @@ module ApplicationHelper
   def active_class(path)
     return_class = ''
     if path.class == Array
+      puts :true
       if path.any? { |str| request.path.include? str }
         return_class = 'active'
       end
     else
       if request.path.include?(path)
+        puts :true
         return_class = 'active'
+      end
+      if  path=='pending_bills' and request.path == '/invoices/pending_bills'
+        return_class = 'active'
+      elsif  path=='collected_bills' and request.path == '/invoices/collected_bills'
+        return_class = 'active'
+      elsif
+        (path=='pending_bills' ) and request.path == '/invoices'
+        return_class = ''
+      elsif
+      (path=='invoices' )
+        return_class = 'active'
+      else
+        (path=='collected_bills' ) and request.path == '/invoices'
+        return_class = ''
       end
     end
     return_class
