7b302174431d760e7df50f298685a90618a58a36
Abirami <abirami@bitcot.com>
2019-08-08 05:51:41 +0530

    500 error fix

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -2,11 +2,9 @@ class InvoicesController < ApplicationController
 include ApplicationHelper
   def index
     invoices_collection = JSON.parse(File.read('public/helpers/new_invoices.json'))
-   if Invoice.includes(:collections).blank?
-     invoices_collection.each do |data|
-       Invoice.where(brand_manager: data['brand_manager'], customer_name: data['customer_name'], narration: data['narration'], amount: data['amount'], reference: data['reference'], invoice_date: data['invoice_date']).first_or_create
-     end
-   end
+    invoices_collection.each do |data|
+      Invoice.where(brand_manager: data['brand_manager'], customer_name: data['customer_name'], narration: data['narration'], amount: data['amount'], reference: data['reference'], invoice_date: data['invoice_date']).first_or_create
+    end
     @invoices = render_cached_json("invoices_list", {expires_in: 1.hour}) do
       Invoice.includes(:collections)
     end
