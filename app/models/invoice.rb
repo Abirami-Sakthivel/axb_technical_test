ffe3ec4c2bc4d8ea6a4a22434731123a47e6cbee
Abirami <abirami@bitcot.com>
2019-08-08 07:53:19 +0530

    500 fix

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,18 +2,18 @@ require 'redis-rails'
 class Invoice < ApplicationRecord
   has_many :collections
 
-  after_save :write_cache
+  # after_save :write_cache
 
   validates_uniqueness_of :reference
 
 
-  def write_cache
-    unless Rails.cache.fetch('invoices_list').blank?
-      json = JSON.parse(Rails.cache.fetch('invoices_list')).map { |k, str|  k and k['reference'] == self.reference  ? k = JSON.parse(self.to_json) : k}.to_json
-      JSON.parse(Rails.cache.write('invoices_list', json))
-    end
-
-  end
+  # def write_cache
+  #   unless Rails.cache.fetch('invoices_list').blank?
+  #     json = JSON.parse(Rails.cache.fetch('invoices_list')).map { |k, str|  k and k['reference'] == self.reference  ? k = JSON.parse(self.to_json) : k}.to_json
+  #     JSON.parse(Rails.cache.write('invoices_list', json))
+  #   end
+  #
+  # end
 
   # def clear_cache
   #   Rails.cache.redis.del('invoices_list')
